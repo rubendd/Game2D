ccb39912e35cd41857ff2e11be76cb692954e000
rubee <rubeendugo@gmail.com>
2023-09-26 20:56:52 +0200

    fix jump

--- Hero.cpp
+++ Hero.cpp
@@ -1,48 +1,46 @@
 #include "Hero.h"
 
-
-Hero::Hero(){}
-
-
-Hero::~Hero(){}
-
-void Hero::init(std::string textureName, sf::Vector2f position, float mass){
-
-	m_position = position;
-	m_mass = mass;
-
-	m_grounded = false;
-	
-	// Load a Texture
-	m_texture.loadFromFile(textureName.c_str());
-
-	// Create Sprite and Attach a Texture
-	m_sprite.setTexture(m_texture);
-	m_sprite.setPosition(m_position);
-	m_sprite.setOrigin(m_texture.getSize().x / 2, m_texture.getSize().y / 2);
+Hero::Hero() {
 
 }
 
-void Hero::update(float dt){
+Hero::~Hero() {
 
+}
+
+void Hero::init(std::string textureName, sf::Vector2f posicion, float mass) {
+	m_position = posicion;
+	m_mass = mass;
+	m_grounded = false;
+
+	//Load texture.
+	m_texture.loadFromFile(textureName.c_str());
+
+	//Crear sprite y attach a texture.
+	m_sprite.setTexture(m_texture);
+	m_sprite.setPosition(m_position);
+	m_sprite.setOrigin(m_texture.getSize().x / 2, m_texture.getSize().y / 2);
+}
+
+//Updatear por cada frame.
+void Hero::update(float dt) {
 	m_velocity -= m_mass * m_gravity * dt;
 
 	m_position.y -= m_velocity * dt;
 
 	m_sprite.setPosition(m_position);
 
-
-	if (m_position.y >= 768 * 0.75) {
-
+	if (m_position.y >= 768 * 0.75)
+	{
 		m_position.y = 768 * 0.75;
 		m_velocity = 0;
 		m_grounded = true;
 		jumpCount = 0;
 	}
-
 }
 
-void Hero::jump(float velocity){
+//Salto
+void Hero::jump(float velocity) {
 
 	if (jumpCount < 2) {
 		jumpCount++;
@@ -53,7 +51,8 @@ void Hero::jump(float velocity){
 
 }
 
-sf::Sprite Hero::getSprite(){
+//Retornar sprite.
+sf::Sprite Hero::getSprite() {
 
 	return m_sprite;
 }
